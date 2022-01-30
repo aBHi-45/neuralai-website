c6dfeb3db6c711f28040cec34e870aa11f92132c
Abhishek Kumar <abhi287786@gmail.com>
2022-01-30 15:16:21 +0530

    Made Responsive

--- src/common/Footer/Footer.js
+++ src/common/Footer/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Container, Typography, makeStyles, Grid, Link } from '@material-ui/core';
+import { Container, Typography, Grid, Link } from '@material-ui/core';
 import { motion } from 'framer-motion';
+import './footerResponsive.css';
+import { StylesProvider } from '@material-ui/styles';
 
 const introVariants = {
     initial: {
@@ -17,19 +19,7 @@ const introVariants = {
     }
 }
 
-const useStyles = makeStyles({
-    links: {
-        color: '#d7d7d7',
-        textAlign: 'center',
-        fontSize: '1.5vw',
-        '&:hover': {
-            color: '#00b7eb'
-        }
-    }
-})
-
 const Footer = () => {
-    const classes = useStyles();
     const links = [
         {
             text: 'About',
@@ -73,24 +63,22 @@ const Footer = () => {
             initial='initial'
             whileInView='inView'
         >
-            <Container style={{ background: '#141414', paddingBottom: '4.75rem', width: '100%' }}>
-                <hr/>
-                <Grid container spacing={7} style={{ marginTop: '2.2rem' }}>
+            <StylesProvider injectFirst>
+            <Container style={{ background: '#141414', paddingBottom: '1rem', width: '100%' }}>
+                <hr />
+                <Grid container style={{ marginTop: '2.2rem' }}>
                     {/* NeuralAI Intro */}
-                    <Grid item sm={12} md={8} spacing={10}>
+                    <Grid item sm={12} md={8} spacing={10} className='containers' gutterBottom>
                         <Typography
                             variant='caption'
                             color='primary'
-                            style={{ fontSize: '2.8vw', lineHeight: '3.4vw' }}
+                            className='introNeuralAI'
                         >
                             NeuralAI aims to bring the power of&#160;
                             <Typography
                                 variant='caption'
                                 color='textPrimary'
-                                style={{ fontSize: '2.8vw', lineHeight: '3.4vw' }}
-                                variants={introVariants}
-                                animate='inView'
-                                initial='initial'
+                                className='introNeuralAI'
                             >
                                 Artificial Intelligence to DTU
                             </Typography>
@@ -99,12 +87,12 @@ const Footer = () => {
                     </Grid>
 
                     {/* NeuralAI Links */}
-                    <Grid item xs={12} sm={6} md={2}>
+                    <Grid item xs={12} sm={6} md={2} className='containers'>
                         {links.map((link) => {
                             return (
-                                <Link href={link.href} style={{textDecoration:'none'}}>
+                                <Link href={link.href} style={{ textDecoration: 'none' }}>
                                     <Typography
-                                        className={classes.links}
+                                        className='links'
                                         variant='body1'
                                         textAlign='center'
                                         gutterBottom
@@ -117,49 +105,52 @@ const Footer = () => {
                     </Grid>
 
                     {/* NeuralAI Social Media */}
-                    <Grid item xs={12} sm={6} md={2}>
+                    <Grid item xs={12} sm={6} md={2} className='containers'>
                         {social.map((sm) => {
                             return (
-                                <a href={sm.href} style={{textDecoration:'none'}}>
+                                <Link href={sm.href} style={{ textDecoration: 'none' }}>
                                     <Typography
-                                        className={classes.links}
+                                        className='links'
                                         variant='body1'
                                         textAlign='center'
                                         gutterBottom
                                     >
                                         {sm.text}
                                     </Typography>
-                                </a>
+                                </Link>
                             )
                         })}
                     </Grid>
 
                     {/* NeuralAI Logo */}
-                    <Grid item sm={6} spacing={10}>
-                        <img style={{ width: '60%', padding: 20 }} src={require('../../img/logoFooter.png')} />
+                    <Grid item sm={6} style={{textAlign: 'center'}} className='containerLogo'>
+                        <img className='logo' alt='NeuralAI-logo' src={require('../../img/logoFooter.png')} />
                     </Grid>
 
                     {/* NeuralAI Email & Rights */}
-                    <Grid item sm={6} spacing={10}>
-                        <br />
-                        <br />
-                        <Typography
-                            variant='body1'
-                            color='secondary'
-                            style={{ textAlign: 'right', fontSize: '1.8vw', padding: 20, paddingBottom: 0 }}
-                        >
-                            neural.ai.dtu@gmail.com
-                        </Typography>
+                    <Grid item sm={6} spacing={10} style={{textAlign:'right'}} className='containers'>
+                        <br/>
+                        <br/>
+                        <Link href='mailto:neural.ai.dtu@gmail.com' style={{ textDecoration: 'none' }}>
+                            <Typography
+                                variant='body1'
+                                color='secondary'
+                                className='mail'
+                            >
+                                neural.ai.dtu@gmail.com
+                            </Typography>
+                        </Link>
                         <Typography
                             variant='body1'
                             color='primary'
-                            style={{ textAlign: 'right', fontSize: '1.8vw', padding: 20, paddingTop: 0 }}
+                            className='mail'
                         >
                             &copy; 2022 NeuralAI DTU. All Rights Reserved
                         </Typography>
                     </Grid>
                 </Grid>
             </Container>
+            </StylesProvider>
         </motion.div>
     )
 }
