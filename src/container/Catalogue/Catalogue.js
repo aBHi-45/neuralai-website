6cd3fa4d0e28adb80b8e417ecfc2200bac0fa6fb
aBHi-45 <abhi287786@gmail.com>
2022-04-17 00:58:01 +0530

    Catalogue

--- src/container/Catalogue/Catalogue.js
+++ src/container/Catalogue/Catalogue.js
@@ -3,11 +3,81 @@ import React from "react";
 import Community from "./Community";
 import Research from "./Research";
 
+const imgVariants = {
+    right: {
+        initial: {
+            x: '5vw',
+            opacity: 0,
+        },
+        inView: {
+            x: 0,
+            opacity: 1,
+            transition: {
+                type: 'tween',
+                duration: 0.5
+            }
+        },
+        exit: {
+            y: '-5vw',
+            opacity: 0,
+            transition: {
+                type: 'tween',
+                duration: 0.5
+            }
+        }
+    },
+    left: {
+        initial: {
+            x: '-5vw',
+            opacity: 0,
+        },
+        inView: {
+            x: 0,
+            opacity: 1,
+            transition: {
+                type: 'tween',
+                duration: 0.5
+            }
+        },
+        exit: {
+            y: '-5vw',
+            opacity: 0,
+            transition: {
+                type: 'tween',
+                duration: 0.5
+            }
+        }
+    }
+}
+
+const textVariants = {
+    initial: {
+        y: '5vw',
+        opacity: 0
+    },
+    inView: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            type: 'tween',
+            duration: 0.5
+        }
+    },
+    exit: {
+        y: '-5vw',
+        opacity: 0,
+        transition: {
+            type: 'tween',
+            duration: 0.5
+        }
+    }
+}
+
 const Catalogue = () => {
     return (
         <Container>
-            <Research />
-            <Community/>
+            <Research variants={imgVariants.left} text ={textVariants}/>
+            <Community variants={imgVariants.right} text={textVariants}/>
         </Container>
     )
 }
--- src/container/Catalogue/Community.js
+++ src/container/Catalogue/Community.js
@@ -1,47 +1,67 @@
-import { Button, Container, Grid, Typography } from '@material-ui/core';
+import { Button, Container, Grid, Typography } from "@material-ui/core";
 import React from "react";
-import './catalogue.css'
+import "./catalogue.css";
+import { motion } from "framer-motion";
 
-const Community = () => {
-    return (
-        <Container>
-            <Grid container style={{ margin: '2.5rem' }}>
-                <Grid item sm={6} gutterBottom style={{padding:'5%'}}>
-                    <Typography
-                        variant='h1'
-                        color='secondary'
-                        style={{ textAlign: 'left', paddingTop: '4rem', fontSize: '3.5rem', paddingBottom: '3rem', fontWeight: 'bold'}}
-                    >
-                        Community
-                    </Typography>
-                    <Typography
-                        variant='body'
-                        color='secondary'
-                        style={{fontSize: '1.3rem'}}
-                    >
-                        Community members are encouraged to express their ideas, think inquisitively, ask questions and form Special Interest Groups to work on different topics.
-                    </Typography>
-                    <br/>
-                    <Button
-                        href='#Community'
-                        variant='outlined'
-                        style={{ marginTop: '3rem', padding:'1rem'}}
-                        color='secondary'
-                        size='large'
-                    >
-                        Learn More
-                    </Button>
-                </Grid>
-                <Grid item sm={6}>
-                    <img
-                    className="section-img"
-                    alt="Community Logo"
-                    src={require("../../img/C-community.jpg")}    
-                    />
-                </Grid>
-            </Grid>
-        </Container>
-)
-}
+const Community = (props) => {
+  return (
+    <Container>
+      <Grid container style={{ margin: "2.5rem" }}>
+        <Grid item sm={6} gutterBottom style={{ padding: "5%" }}>
+          <motion.div
+            variants={props.text}
+            initial="initial"
+            whileInView="inView"
+            exit="exit"
+          >
+            <Typography
+              variant="h1"
+              color="secondary"
+              style={{
+                textAlign: "left",
+                paddingTop: "4rem",
+                fontSize: "3.5rem",
+                paddingBottom: "3rem",
+                fontWeight: "bold",
+              }}
+            >
+              Community
+            </Typography>
+            <Typography
+              variant="body"
+              color="secondary"
+              style={{ fontSize: "1.3rem" }}
+            >
+              Community members are encouraged to express their ideas, think
+              inquisitively, ask questions and form Special Interest Groups to
+              work on different topics.
+            </Typography>
+            <br />
+            <Button
+              href="#Community"
+              variant="outlined"
+              style={{ marginTop: "3rem", padding: "1rem" }}
+              color="secondary"
+              size="large"
+            >
+              Learn More
+            </Button>
+          </motion.div>
+        </Grid>
+        <Grid item sm={6}>
+          <motion.img
+            className="section-img"
+            alt="Community Logo"
+            src={require("../../img/C-community.jpg")}
+            variants={props.variants}
+            initial="initial"
+            whileInView="inView"
+            exit="exit"
+          />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
 export default Community;
